0161b477427297673e923678e012a42e0f6d36b9
Mo Zongran <mozongran@gmail.com>
2019-12-19 12:31:47 +0800

    Day 1 part 2

--- day_1/main.go
+++ day_1/main.go
@@ -8,16 +8,38 @@ import (
 )
 
 func main() {
-	fuel()
+	// fuel()
+	advFuel()
 }
 
+// fuel calculates the amount of fuel required for part 1
 func fuel() {
 	reader := bufio.NewScanner(os.Stdin)
-	fuel := 0;
-	for reader.Scan(){
+	fuel := 0
+	for reader.Scan() {
 		i, _ := strconv.Atoi(reader.Text())
-		fuel += i / 3 - 2
+		fuel += i/3 - 2
 	}
 
 	fmt.Println(fuel)
+}
+
+// advFuel calculates the amount of fuel required for part 2
+func advFuel() {
+	reader := bufio.NewScanner(os.Stdin)
+	fuel := 0
+	for reader.Scan() {
+		i, _ := strconv.Atoi(reader.Text())
+		fuel += recFuel(i)
+	}
+	fmt.Println(fuel)
+}
+
+// recFuel recursively calculates the amount of fuel required by the fuel
+func recFuel(fuel int) int {
+	fuelReq := fuel/3 - 2
+	if fuelReq <= 0 {
+		return 0
+	}
+	return fuelReq + recFuel(fuelReq)
 }
